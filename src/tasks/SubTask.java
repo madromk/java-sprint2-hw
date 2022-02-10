59a081bead80aecc75425c856becf4d3580a3efa
Roman <maderomk@yandex.ru>
2022-02-10 22:56:06 +0300

    Добавлена переменная для хранения id Эпика

--- src/tasks/SubTask.java
+++ src/tasks/SubTask.java
@@ -1,7 +1,21 @@
+package tasks;
+
+import tasks.BaseTask;
+
 public class SubTask extends BaseTask {
+
+    private int belongEpicId;
 
     public SubTask(String name, String description, String status) {
         super(name, description, status);
+    }
+
+    public void setBelongEpicId(int belongEpicId) {
+        this.belongEpicId = belongEpicId;
+    }
+
+    public int getBelongEpicId() {
+        return belongEpicId;
     }
 }
 
