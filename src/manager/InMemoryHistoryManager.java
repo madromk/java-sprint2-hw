8c6be9a9cea71ee243ab4d31a68dd49adab939dc
Roman <maderomk@yandex.ru>
2022-02-18 17:23:40 +0300

    Изменен метод history

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -7,16 +7,16 @@ import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
+    private static final int MAX_SIZE_HISTORY = 10;
+
     private List<BaseTask> history = new ArrayList<>();
 
     @Override
     public void addInHistory(BaseTask task) {
-        if(history.size() <= 10) {
-            history.add(task);
-        } else {
+        if(history.size() == MAX_SIZE_HISTORY) {
             history.remove(0);
-            history.add(task);
         }
+        history.add(task);
     }
 
     @Override
